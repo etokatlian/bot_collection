c558884123a322e76b85c24c07aa34bf585a0afa
etokatlian <etokatlian@gmail.com>
2023-02-10 23:05:01 -0700

    Initial commit

---
+++ test/github_bot.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as GithubBot from '../lib/github_bot-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/github_bot-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new GithubBot.GithubBotStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
